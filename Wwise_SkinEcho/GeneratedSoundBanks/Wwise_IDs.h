6c5ded9cb756975be641b3685d2e311df8126c3b
RaphSD <33368306+RaphSD@users.noreply.github.com>
2018-04-27 14:29:41 +0200

    Add Wwise Header

---
+++ Wwise_SkinEcho/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_NIVEAU_OX_5 = 495794050U;
+        static const AkUniqueID PLAY_NIVEAU_OX_10 = 189687746U;
+        static const AkUniqueID PLAY_NIVEAU_OX_15 = 189687751U;
+        static const AkUniqueID PLAY_NIVEAU_OX_25 = 172910186U;
+        static const AkUniqueID PLAY_NIVEAU_OX_50 = 122577302U;
+        static const AkUniqueID PLAY_NIVEAU_OX_75 = 89022133U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_004 = 3851696881U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_005 = 3851696880U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_006 = 3851696883U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_007 = 3851696882U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_008 = 3851696893U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_009 = 3851696892U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_010 = 3834919170U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_011 = 3834919171U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_012 = 3834919168U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_AI_001 = 65657337U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_AI_002 = 65657338U;
+        static const AkUniqueID PLAY_PROTO_2_VOIX_AI_003 = 65657339U;
+        static const AkUniqueID PLAY_RADIO_VOICE_START = 2019705941U;
+        static const AkUniqueID PLAY_SFX_AMBIANCE_01 = 1281001716U;
+        static const AkUniqueID PLAY_SFX_BUTTON_PRESS = 1223272146U;
+        static const AkUniqueID PLAY_SFX_MONSTER_CALM_01 = 3773561252U;
+        static const AkUniqueID PLAY_SFX_WATER_BREACH = 2798239405U;
+        static const AkUniqueID PLAY_SFX_WELDER_TAKE = 1487639259U;
+        static const AkUniqueID PLAY_SFX_WELDING = 3723595182U;
+        static const AkUniqueID PLAY_SFX_WELDING_AIR = 3575465515U;
+        static const AkUniqueID STOP_RADIO_VOICE_START = 1243176019U;
+        static const AkUniqueID STOP_SFX_WATER_BREACH = 350557031U;
+        static const AkUniqueID STOP_SFX_WELDING = 1302341568U;
+        static const AkUniqueID STOP_SFX_WELDING_AIR = 1297865509U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_BREACH_SIZE = 1522755476U;
+        static const AkUniqueID RTPC_MUSIC_VOLUME = 1596647065U;
+        static const AkUniqueID RTPC_SFX_VOLUME = 932301089U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SE_MAIN_SOUNDBANK = 412588681U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_5_1 = 4280977430U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VOICE = 3170124113U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB_MONSTER = 1049073410U;
+        static const AkUniqueID REVERB_SUBMARINE = 998994920U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID COMMUNICATION = 530303819U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
